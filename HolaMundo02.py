eb84697d612c9099c65a566aa88a236ca57b1301
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-18 23:58:26 -0500

    consultar  tipos de datos

---
+++ HolaMundo02.py
@@ -0,0 +1,7 @@
+# imprime el tipo de dato de un valor
+print("TIPO DE DATO DE HOLA MUNDO: ")
+print(type("Hola Mundo")) #tipo str
+print(type(3)) #tipo int
+print(type(3.4)) #tipo float
+print(type(3.4)) #tipo float
+print(type(3 + 1j)) #tipo complex

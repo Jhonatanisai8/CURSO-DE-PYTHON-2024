b15f6a5b2a14d7c1ed08fc29976c20cffae188a2
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-22 22:31:38 -0500

    Variables

---
+++ 03Variables.py
@@ -0,0 +1,30 @@
+# VARIABLES
+print("Variables en Python")
+
+my_variable = "Esta es mi variable"
+my_edad = 3
+my_boo = True
+
+print(my_variable)
+print(my_edad)
+print(my_boo)
+
+# concatenacion de variables
+print(my_variable,my_edad,my_boo)
+
+print("Convertir int a str")
+numero_celular = str(my_edad)
+print(numero_celular)
+print(type(numero_celular))
+
+#funciones del sistema
+my_nombre = "Jhonatan"
+print(len(my_nombre))
+
+my_luga_nacimiento = "Sullana"
+print("Este es el valor de: ",my_luga_nacimiento)
+
+# variables en una sola linea
+nombre, apellido, alias, edad = "Jhonatan Isai", "Ojeda Sanchez", "Jhomy", 18
+
+print("Me llamo: ",nombre,apellido,". Mi edad es: ",edad," y mi apodo es: ",alias)

617e856145eae07380ff459eb3b42435424ad6e7
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-22 22:58:47 -0500

    Ejercicio 02: Compara la longitud de tu nombre y tu apellido

---
+++ Ejercicios Variables/02EjercicioCompararApellidoNombre.py
@@ -0,0 +1,12 @@
+nombre = "Jhonatan"
+apellido = "Sanchez"
+
+longitud_nombre = len(nombre)
+longitud_apellido = len(apellido)
+
+#motramos
+print(len(nombre))
+print(len(apellido))
+
+# comparamos
+print("Son iguales las cadenas: ",longitud_apellido == longitud_nombre)

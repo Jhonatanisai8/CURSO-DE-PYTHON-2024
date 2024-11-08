c7b847c597ad42ebf17d2a2a243db46c387e787e
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-07 22:46:32 -0500

    Operadores aritmeticos

---
+++ 05Operadores.py
@@ -0,0 +1,13 @@
+print("OPERADORES EN PYTHON")
+print("SUMA: ",1+4)
+print("RESTA: ",6-4)
+print("DIVISION: ",8/4)
+print("MULTIPLICACION: ",4*4)  
+print("MODULO: ",5%4)  
+print("DIVICION SIN EL RESTO: ",4//3)  
+print("POTENCIA: ", 2 ** 3)  
+
+print("Jhonatan"+" Isai")
+print("Hola " * 3)
+
+

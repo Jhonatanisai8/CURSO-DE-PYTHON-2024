3f715400d7610a4fe181933c47ece68700f05830
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-07 22:47:51 -0500

    Ejemplo de variables

---
+++ ejemplo.py
@@ -0,0 +1,33 @@
+edad = 30
+nombre = "Juan"
+altura = 1.75
+vive_solo = True
+
+edad,nombre,altura,vive_solo = 12,"Pedro",1.80,False
+
+#entero
+edad = 43
+
+#float
+precio_producto = 34.5
+
+#cadenas
+nombre_completo = "Samir Aldair Juarez"
+
+#listas
+lista = ["Mateo Diaz","Hola Mundo",12,True,4.6]
+
+#tupla
+tupla = ("Mateo Diaz","Hola Mundo",12,True,4.6,)
+
+#conjunto
+conjunto = {"Mateo Diaz","Hola Mundo",12,True,4.6}
+
+#diccionario
+diccionario = {
+    'nombre': "Mateo Alonso",
+    'apellido': "Juarez Lopez",
+    'esta_alegre': True,
+    'escala' : 12.4,
+    'dato_duplicado': "Mateo Alonso"
+}

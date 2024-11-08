fdcead8b4a219ece3e67a62892c3b447e515fd9a
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-07 22:45:52 -0500

    Ejercicio Operadores

---
+++ Ejercicios Variables/03EjercicioOperaciones.py
@@ -0,0 +1,13 @@
+"""
+Declare 5 como num_one y 4 como num_two
+"""
+num_one = 5
+num_two = 4
+
+#Agregue num_one y num_two y asigne el valor a un total variable
+num_total = num_two + num_one
+print("Nuevo valor total: ",num_total)
+
+#Reste num_two de num_one y asigne el valor a una variable diff
+dif = num_one - num_two
+print("resultado resta: ",dif)

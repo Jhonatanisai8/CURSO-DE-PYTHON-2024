c285be07a1cdfc9cc949cccd19dfdbbd3cace36d
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-11-23 21:02:04 -0500

    bucle while

---
+++ bucles/bucle-while/02-bucle-while.py
@@ -0,0 +1,9 @@
+print("Bucle While")
+
+# mientras que la condicion se cumpla, el bucle se va a seguir ejecutando 
+contador = 0
+while contador <= 10:
+    print(contador)
+    contador+=1
+
+print("el while llego a su fin")

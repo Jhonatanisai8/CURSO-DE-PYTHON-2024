12d5332d6c23af347be040c3622c2e2294f6a752
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-05 10:02:49 -0500

    Iterar listas con while

---
+++ bucles/bucle-while/03-iterrar-lista_while.py
@@ -0,0 +1,7 @@
+print("Iterar lista con while")
+lista_frutas =  ["Limon","Naranga","Mango"]
+print(type(lista_frutas))
+indice = 0
+while  indice < len(lista_frutas):
+    print(lista_frutas[indice])
+    indice+=1
